18bd31e2b786ddd4ab3d75e7ada86c447c3b513a
Benny Chan <bennyx93@tamu.edu>
2021-12-22 11:30:55 -0600

    State management using context

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { CounterButton } from './CounterButton';
+import { CounterProvider } from './CounterProvider';
+import './App.css';
+
+const App = () => {
+	return (
+		<CounterProvider>
+			<h1>State Management Example</h1>
+			<CounterButton />
+		</CounterProvider>
+	);
+}
+
+export default App;
